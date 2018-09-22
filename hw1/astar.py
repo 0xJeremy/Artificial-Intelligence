8b6f24224130d8a360c9e5fb06d2f27e48f37e0e
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-09-22 15:22:14 -0400

    Currently Unworking A*

--- hw1/astar.py
+++ hw1/astar.py
@@ -1,8 +1,23 @@
 import math, sys, os
 from Queue import *
 import networkx as nx
+import heapq
+import copy
 
 NUM_PANCAKES = 5
+
+class pQueue:
+	def __init__(self):
+		self.elements = []
+
+	def empty(self):
+		return len(self.elements) == 0
+
+	def put(self, element, priority):
+		heapq.heappush(self.elements, (priority, element))
+
+	def get(self):
+		return heapq.heappop(self.elements)[1]
 
 class Pancakes:
 	def __init__(self, stack, back_cost):
@@ -10,6 +25,7 @@ class Pancakes:
 		self.back_cost = back_cost
 		self.size = len(stack)-1
 		self.forward_cost = self.heuristic()
+		self.total_cost = self.back_cost + self.forward_cost
 
 	def check_order(self):
 		for x in range(self.size):
@@ -37,27 +53,52 @@ class Pancakes:
 			location -= 1
 
 def astar(pancakes):
-	open_list = Queue(maxsize=0)
-	closed_list = Queue(maxsize=0)
+	open_list = pQueue()
+	open_list.put(pancakes, pancakes.total_cost)
+
+	closed_list = set()
+
 	G = nx.Graph()
-
-
-
 	G.add_node(pancakes)
-	open_list.put(pancakes)
 
-	while(!open_list.empty()):
+	while(not open_list.empty()):
 		current = open_list.get()
+		closed_list.add(current)
 
-		if(current.check_order):
-			return current
-
-		for x in range(len(current.size)):
-			temp = copy.deepcopy(p)
+		for x in range(current.size):
+			temp = copy.deepcopy(pancakes)
 			temp.back_cost += 1
 			temp.flip(x)
+			if(current.check_order):
+				break
+			if(temp in closed_list):
+				continue
 			G.add_node(temp)
 			G.add_edge(current, temp)
+
+	return closed_list
+
+
+
+	# open_list = Queue(maxsize=0)
+	# closed_list = Queue(maxsize=0)
+	# G = nx.Graph()
+
+	# G.add_node(pancakes)
+	# open_list.put(pancakes)
+
+	# while(!open_list.empty()):
+	# 	current = open_list.get()
+
+	# 	if(current.check_order):
+	# 		return current
+
+	# 	for x in range(len(current.size)):
+	# 		temp = copy.deepcopy(p)
+	# 		temp.back_cost += 1
+	# 		temp.flip(x)
+	# 		G.add_node(temp)
+	# 		G.add_edge(current, temp)
 
 
 def main():
@@ -65,6 +106,9 @@ def main():
 	p = Pancakes(stack, 0)
 	print("EXPECTED: 5")
 	print("          " + str(p.forward_cost))
+	closed_list = astar(p)
+	for i in closed_list:
+		print(i)
 
 
 if __name__=='__main__':
