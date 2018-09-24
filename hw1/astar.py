1f6c7ac8a9f29f6fae62e75b39aed45bfc1a1eb8
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-09-24 19:09:45 -0400

    Fixed A*. Currently Working

--- hw1/astar.py
+++ hw1/astar.py
@@ -29,7 +29,7 @@ class Pancakes:
 
 	def check_order(self):
 		for x in range(self.size):
-			if(self.stack[x] < self.stack[x+1]):
+			if(self.stack[x] > self.stack[x+1]):
 				return False
 		return True
 
@@ -57,7 +57,17 @@ class Pancakes:
 		self.forward_cost = self.heuristic()
 		self.total_cost = self.back_cost + self.forward_cost
 
+	def print_stack(self):
+		print_string = "Stack: "
+		for i in range(len(self.stack)):
+			print_string += str(self.stack[i])
+		print(print_string)
+
 def astar(pancakes):
+	print("Running A* on list...")
+	print("Initial List: ")
+	pancakes.print_stack()
+
 	frontier = pQueue()
 	frontier.put(pancakes, pancakes.total_cost)
 
@@ -70,17 +80,21 @@ def astar(pancakes):
 	G = nx.Graph()
 	G.add_node(pancakes)
 
+	counter = 0
+
 	while(not frontier.empty()):
 		current = frontier.get()
 
 		if(current.check_order()):
-			break
+			print("\nFINAL: ")
+			current.print_stack()
+			return visited
 
+		counter += 1
 		for x in range(current.size):
 			temp = copy.deepcopy(current)
 			temp.update_costs()
-			print("Back Cost: " + str(temp.back_cost))
-			temp.flip(x)
+			temp.flip(x+1)
 			if(temp in visited):
 				continue
 			G.add_node(temp)
@@ -88,7 +102,6 @@ def astar(pancakes):
 
 		for x in G.neighbors(current):
 			temp_cost = x.total_cost
-			print("Temp Cost: " + str(temp_cost))
 			if x not in cost or temp_cost < cost[x]:
 				cost[x] = temp_cost
 				frontier.put(x, x.total_cost)
@@ -97,13 +110,15 @@ def astar(pancakes):
 	return visited
 
 def main():
-	stack = [3, 2, 5, 1, 6, 4, 7]
+	#stack = [3, 2, 5, 1, 6, 4, 7]
+	stack = [1, 2, 3, 4, 5, 6, 7, 8]
+	#stack = [3, 2, 1, 4]
 	p = Pancakes(stack, 0)
-	print("EXPECTED: 5")
-	print("          " + str(p.forward_cost))
-	closed_list = astar(p)
-	for i in closed_list:
-		print(i)
+	# print("EXPECTED: 5")
+	# print("          " + str(p.forward_cost))
+	visited = astar(p)
+	# for i in visited:
+	# 	i.print_stack()
 
 
 if __name__=='__main__':
