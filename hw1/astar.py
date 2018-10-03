00b599bca6edeb2d45a88e73276d540c4dd83d58
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-10-03 15:06:41 -0400

    Added README

--- hw1/astar.py
+++ hw1/astar.py
@@ -1,9 +1,9 @@
 import math, sys, os
-from Queue import *
 import networkx as nx
 import heapq
 import copy
 
+# Priority Queue Class
 class pQueue:
 	def __init__(self):
 		self.elements = []
@@ -17,6 +17,7 @@ class pQueue:
 	def get(self):
 		return heapq.heappop(self.elements)[1]
 
+# Pancakes Class
 class Pancakes:
 	def __init__(self, stack, back_cost):
 		self.stack = stack
@@ -25,12 +26,14 @@ class Pancakes:
 		self.forward_cost = self.heuristic()
 		self.total_cost = self.back_cost + self.forward_cost
 
+	# Checks Pancake order
 	def check_order(self):
 		for x in range(self.size):
 			if(self.stack[x] > self.stack[x+1]):
 				return False
 		return True
 
+	# Forward cost algorithm
 	def heuristic(self):
 		cost = 0
 		for x in range(self.size):
@@ -40,6 +43,7 @@ class Pancakes:
 		self.forward_cost = cost
 		return cost
 
+	# Flip function at 'location'
 	def flip(self, location):
 		temp, x = 0, 0
 		while(x < location):
@@ -49,17 +53,20 @@ class Pancakes:
 			x += 1
 			location -= 1
 
+	# Update the forward and backward costs
 	def update_costs(self):
 		self.back_cost += 1
 		self.forward_cost = self.heuristic()
 		self.total_cost = self.back_cost + self.forward_cost
 
+	# Prints to the pancake stack to console
 	def print_stack(self):
 		print_string = "Stack: "
 		for i in range(len(self.stack)):
 			print_string += str(self.stack[i]) + " "
 		print(print_string)
 
+# A* algorithm running on pancakes
 def astar(pancakes):
 	print("Running A* on list...")
 	print("Initial List: ")
@@ -95,6 +102,7 @@ def astar(pancakes):
 			path.reverse()
 			return path, G.number_of_nodes(), total_cost
 
+		# Generates children and places them in graph
 		for x in range(current.size):
 			temp = copy.deepcopy(current)
 			temp.update_costs()
@@ -104,6 +112,8 @@ def astar(pancakes):
 			G.add_node(temp)
 			G.add_edge(current, temp)
 
+			# Checks if child already exists and if new
+			#		path is shorter than previous.
 			temp_cost = temp.total_cost
 			if temp not in cost or temp_cost < cost[temp]:
 				cost[temp] = temp_cost
@@ -113,16 +123,27 @@ def astar(pancakes):
 	return visited
 
 def main():
+
+	# Initial Stack
+	# CHANGE THIS FOR DIFFERENT INPUTS
+
 	stack = [3, 2, 5, 1, 6, 4, 7]
+
+
+
+	# Creates a new pancake and runs A*
 	p = Pancakes(stack, 0)
 	visited, num_nodes, total_cost = astar(p)
 	print("")
+
+	# Prints pancake flips
 	for i in visited:
 		if(i == visited[len(visited)-1]):
 			print("\nFinal Stack:")
 		i.print_stack()
 	print("\n=====Sorting Finished=====\n")
 
+	# Reports algorithm stats
 	print("Number of Nodes: " + str(num_nodes))
 	print("Total Flips: " + str(total_cost))
 
