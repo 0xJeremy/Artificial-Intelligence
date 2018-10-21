3caf28973308e7eb9be147188918908e34213d29
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-10-21 12:05:06 -0400

    Updated code and added README

--- hw2/sudoku.py
+++ hw2/sudoku.py
@@ -8,12 +8,12 @@ numbers = '123456789'
 #***************************************************
 #                 Helper Functions
 #***************************************************
-def constraint(xi, xj):
-	return xi != xj
 
+# Combines the items x and y passed
 def combine(x, y):
 	return [i + j for i in x for j in y]
 
+# Finds all permutations of a combination passed into it
 def permute(combination):
 	result = list()
 	for x in range(0, len(combination) + 1):
@@ -22,6 +22,7 @@ def permute(combination):
 				result.append(subset)
 	return result
 
+# Returns the number of conflicts in a sudoku board
 def conflicts(sudoku, var, val):
 	count = 0
 	for n in sudoku.neighbors[var]:
@@ -29,11 +30,15 @@ def conflicts(sudoku, var, val):
 			count += 1
 	return count
 
+# Function to print the initial state of the board
 def print_initial_board(board):
 	string = ''
 	count = 1
 	for x in board:
-		string = string + x + ' '
+		if(x == '0'):
+			string = string + '  '
+		else:
+			string = string + x + ' '
 		if(count % 9 == 0):
 			string += '\n'
 		elif(count % 3 == 0):
@@ -46,8 +51,11 @@ def print_initial_board(board):
 #***************************************************
 #                 Sudoku Class
 #***************************************************
+
+# Defines the Sudoku class used to contain the board
 class Sudoku:
 
+	# Constructor function which sets the board variables
 	def __init__(self, board):
 		self.variables = []
 		self.domains = {}
@@ -56,6 +64,8 @@ class Sudoku:
 		self.pruned = {}
 		self.initialize(board)
 
+	# Initialies the variables of the sudoku object
+	#	based on the board passed to it
 	def initialize(self, board):
 		game = list(board)
 		self.variables = combine(characters, numbers)
@@ -69,6 +79,7 @@ class Sudoku:
 		self.constrain()
 		self.populate()
 
+	# Defines and sets the constraints of the board
 	def constrain(self):
 		x = ([combine(characters, number) for number in numbers] +
 			[combine(character, numbers) for character in characters] +
@@ -79,6 +90,7 @@ class Sudoku:
 				if([j[0], j[1]] not in self.constraints):
 					self.constraints.append([j[0], j[1]])
 
+	# Populates the object with neighbors
 	def populate(self):
 		for i in self.variables:
 			self.neighbors[i] = []
@@ -86,37 +98,34 @@ class Sudoku:
 				if i == j[0]:
 					self.neighbors[i].append(j[1])
 
+	# Checks if the sudoku game has been solved
 	def solved(self):
 		for x in self.variables:
 			if(len(self.domains[x]) > 1):
 				return False
 		return True
 
-	def complete(self, assignment):
-		for x in self.variables:
-			if len(self.domains[x]) > 1 and x not in assignment:
+	# Checks if the board is consistent in its variables
+	def consistent(self, assignment, var, value):
+		for key, val in assignment.iteritems():
+			if val == value and key in self.neighbors[var]:
 				return False
 		return True
 
-	def consistent(self, assignment, var, value):
-		consistent = True
-		for key, val in assignment.iteritems():
-			if val == value and key in self.neighbors[var]:
-				consistent = False
-
-		return consistent
-
+	# Assigns a value to a specific position
 	def assign(self, var, value, assignment):
 		assignment[var] = value
 		self.forward_check(var, value, assignment)
 
+	# Unassigns a value to a specific position
 	def unassign(self, var, assignment):
 		if var in assignment:
-			for (D, v) in self.pruned[var]:
-				self.domains[D].append(v)
+			for (d, v) in self.pruned[var]:
+				self.domains[d].append(v)
 			self.pruned[var] = []
 			del assignment[var]
 
+	# Checks if domains will be altered due to an assignment
 	def forward_check(self, var, value, assignment):
 		for neighbor in self.neighbors[var]:
 			if neighbor not in assignment:
@@ -127,49 +136,32 @@ class Sudoku:
 #***************************************************
 #                   AC3 Class
 #***************************************************
+
+# Class for the Arc Consistency 3 Algorithm
 class ac3:
+
+	# Constructor for the Algorithm object
 	def __init__(self, board):
 		self.sudoku = Sudoku(board)
 
-	def ac3_solve(self):
+	# Solves the sudoku game
+	def solve(self):
 		if not self.sudoku.solved():
 			assignment = {}
 			for x in self.sudoku.variables:
 				if len(self.sudoku.domains[x]) == 1:
 					assignment[x] = self.sudoku.domains[x][0]
-
 			assignment = self.backtrack(assignment)
 			for d in self.sudoku.domains:
 				self.sudoku.domains[d] = assignment[d] if len(d) > 1 else sudoku.domains[d]
 			if not assignment:
 				print("Error: No solution to Sudoku board.")
 
-	def solve(self):
-		queue = list(self.sudoku.constraints)
-		while(len(queue) != 0):
-			xi, xj = queue.pop(0)
-			if revise(xi, xj):
-				if(len(self.sudoku.domains[xi]) == 0):
-					return False
-				for xk in self.sudoku.neighbors[xi]:
-					if xk != xi:
-						queue.append([xk, xi])
-		return True
-
-	def revise(self, xi, xj):
-		temp = False
-		for x in self.sudoku.domains[xi]:
-			if not any([self.sudoku.constraint(x, y) for y in self.sudoku.domains[xj]]):
-				self.sudoku.domains[xi].remove(x)
-				temp = True
-		return temp
-
+	# Backtrack function to reduce conflicts
 	def backtrack(self, assignment):
 		if len(assignment) == len(self.sudoku.variables):
 			return assignment
-
 		var = self.select_unassigned_variable(assignment)
-
 		for value in self.order_domain_values(var):
 			if self.sudoku.consistent(assignment, var, value):
 				self.sudoku.assign(var, value, assignment)
@@ -188,7 +180,8 @@ class ac3:
 			return self.sudoku.domains[var]
 		return sorted(self.sudoku.domains[var], key=lambda val: conflicts(self.sudoku, var, val))
 
-	def print_board(self):
+	# Prints the completed Sudoku board
+	def print_solved_board(self):
 		string = ''
 		count = 1
 		for var in self.sudoku.variables:
@@ -206,8 +199,13 @@ def main():
 
 	# SWITCH BETWEEN SUDOKU BOARDS HERE ---------------------------------------------------------
 
+	# Easy
 	# board = '608702100400010002025400000701080405080000070509060301000006750200090008006805203'
+
+	# Hard
 	# board = '070042000000008610390000007000004009003000700500100000800000076054800000000610050'
+
+	# Very Hard
 	board = '800000000003600000070090200050007000000045700000100030001000068008500010090000400'
 
 	#--------------------------------------------------------------------------------------------
@@ -215,14 +213,14 @@ def main():
 	print("Initial Board:")
 	print_initial_board(board)
 
-	solved_board = ac3(board)
+	game_board = ac3(board)
 
 	start_time = time.time()
-	solved_board.ac3_solve()
+	game_board.solve()
 	end_time = time.time()
 
 	print("Final Board:")
-	solved_board.print_board()
+	game_board.print_solved_board()
 
 	print("Solve Time: " + str(end_time - start_time) + " seconds")
 
