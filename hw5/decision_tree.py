ece7609e49a8ea0c20d91b0d29dee9caf89cb504
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-12-13 21:49:25 -0500

    Fixed Bugs

--- hw5/decision_tree.py
+++ hw5/decision_tree.py
@@ -9,35 +9,40 @@ BLACKBOARD = {"BATTERY_LEVEL": 100,
 
 class Roomba():
 	def __init__(self):
-		self.battery_1()
-		self.spot_check()
-		self.clean_until_fail()
+		while(True):
+			self.battery_1()
+			self.spot_check()
+			self.clean_until_fail()
 
 	def battery_1(self):
 		if(BLACKBOARD["BATTERY_LEVEL"] < 30):
-			BLACKBOARD.HOME_PATH = [0, 1, 2, 3]
+			BLACKBOARD["HOME_PATH"] = [0, 1, 2, 3]
 			print("Home Path Set")
 			print("Going Home")
 			print("Docking")
+			exit(1)
 
 	def spot_check(self):
 		if(BLACKBOARD["SPOT"]):
 			for i in range(20):
-				sleep(1)
+				# sleep(1)
 				if(i % 5 == 0):
 					print("Cleaning Spot " + str(100 * i / 20) + "%")
+			print("Cleaning Spot 100%")
 			BLACKBOARD["SPOT"] = False
 
 	def clean_until_fail(self):
 		if(BLACKBOARD["GENERAL"]):
-			while(BLACKBOARD["BATTERY_LEVEL"] > 30):
+			while(BLACKBOARD["BATTERY_LEVEL"] >= 30):
 				if(BLACKBOARD["DUSTY_SPOT"]):
 					for i in range(35):
-						sleep(1)
+						# sleep(1)
 						if(i % 5 == 0):
 							print("Cleaning Dusty Spot " + str(100 * i / 35) + "%")
+					print("Cleaning Dusty Spot 100%")
 					BLACKBOARD["DUSTY_SPOT"] = False
 				BLACKBOARD["BATTERY_LEVEL"] -= 5
+				print("Battery: " + str(BLACKBOARD["BATTERY_LEVEL"]))
 			BLACKBOARD["GENERAL"] = False
 
 
