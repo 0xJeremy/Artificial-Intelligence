88e6213646fcb4651aa36da331ad84c38b1bfaa0
0xJeremy <jeremy.kanovsky@tufts.edu>
2018-12-15 13:50:17 -0500

    Added Comments

--- hw5/decision_tree.py
+++ hw5/decision_tree.py
@@ -36,11 +36,12 @@ def sprint(operation, state, battery):
 
 def running(operation, time):
 	operation = operation.ljust(24)
-	for i in range(time):
+	for i in range(time - 1):
 		sleep(1)
 		BLACKBOARD["BATTERY_LEVEL"] -= 1
-		if(i % 5 == 0):
-			print(operation + ". . .   RUNNING (%2d%%)" % (100 * i / time))
+		print(operation + ". . .   RUNNING (%2d%%)" % (100 * i / time), end = '\r')
+	sleep(1)
+	print(operation + ". . .   RUNNING (100%)")
 
 #********************************************************************
 #						Roomba Class
@@ -49,13 +50,16 @@ def running(operation, time):
 class Roomba():
 	def __init__(self):
 		print("\033[1m======== Initializing Roomba ========\033[0m\n")
+
+		# Main runtime loop for decision tree
 		while(True):
-			self.battery_1()
+			self.battery()
 			self.spot_check()
 			self.clean_until_fail()
 			randomize_environment()
 
-	def battery_1(self):
+	# Checks if Roomba needs recharging
+	def battery(self):
 		if(BLACKBOARD["BATTERY_LEVEL"] < 30):
 			sprint("Find Home", "SUCCEEDED", False)
 			BLACKBOARD["HOME_PATH"] = "PATH_TO_DOCKING_STATION"
@@ -64,6 +68,7 @@ class Roomba():
 			print()
 			BLACKBOARD["BATTERY_LEVEL"] = random.randint(80, 100)
 
+	# Performs a spot cleaning, if necessary
 	def spot_check(self):
 		if(BLACKBOARD["SPOT"]):
 			running("Spot Cleaning", 20)
@@ -74,6 +79,7 @@ class Roomba():
 			sprint("Spot", "FAILED", False)
 			print()
 
+	# Main cleaning loop, runs until battery falls below 30%
 	def clean_until_fail(self):
 		if(BLACKBOARD["GENERAL"]):
 			print("General Cleaning        . . .   RUNNING")
